5e304153a838845789aa27ebffa9e88383a4ec6c
Shorina, Varvara <shorivar@fel.cvut.cz>
2021-11-02 16:33:30 +0000

    Update EveningStrategy.java

--- c6/cz/cvut/fel/omo/cv6/strategy/EveningStrategy.java
+++ c6/cz/cvut/fel/omo/cv6/strategy/EveningStrategy.java
@@ -1,2 +1,33 @@
+package cz.cvut.fel.omo.cv6.strategy;
 
+import cz.cvut.fel.omo.cv6.Street;
+import cz.cvut.fel.omo.cv6.TrafficLight;
+
+public class EveningStrategy extends Strategy{
+
+    int time = 0;
+
+    public EveningStrategy(Street street) {
+        super(street);
+    }
+
+    @Override
+    public void controlTraffic() {
+
+        int counter = 0;
+
+        for (TrafficLight light : street.getLights()) {
+
+            if (time - counter * lightDistance == 0) {
+                light.startGoSequence();
+            } else {
+                light.timeLapseTick();
+            }
+            counter++;
+        }
+        time++;
+
+    }
+
+}
 // TODO - TO BE IMPLEMENTED

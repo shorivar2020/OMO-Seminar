ff78dca7fd65bf0bf8d401efa26da471ac85b515
Shorina, Varvara <shorivar@fel.cvut.cz>
2021-11-02 16:30:16 +0000

    Upload New File

---
+++ c6/cz/cvut/fel/omo/cv6/state/Context.java
@@ -0,0 +1,12 @@
+package cz.cvut.fel.omo.cv6.state;
+
+/** Kurz A7B36OMO - Objektove modelovani - Cviceni 6 Design Patterns State, strategy
+ *
+ *  @author mayerto1
+ *
+ *
+ */
+public interface Context {
+
+    void setState(State state);
+}

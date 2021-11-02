faf5edb013e54cebe5113878f6fdc7c1a6f8bdcb
Shorina, Varvara <shorivar@fel.cvut.cz>
2021-11-02 15:13:02 +0000

    Upload New File

---
+++ c5/p1/ExtendedStackImpl.java
@@ -0,0 +1,41 @@
+package cz.cvut.fel.omo;
+
+/**
+ * TODO: implement class using adapter pattern
+ */
+public class ExtendedStackImpl implements ExtendedStack {
+    @Override
+    public void push(int toInsert) {
+
+    }
+
+    @Override
+    public void push(int[] toInsert) {
+
+    }
+
+    @Override
+    public int top() {
+        return 0;
+    }
+
+    @Override
+    public int pop() {
+        return 0;
+    }
+
+    @Override
+    public int popFirstNegativeElement() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+}

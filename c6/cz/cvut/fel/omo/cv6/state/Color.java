fc070ba5dd50bc6e65ec28a4fc9eb7594312c6e6
Shorina, Varvara <shorivar@fel.cvut.cz>
2021-11-02 16:30:07 +0000

    Upload New File

---
+++ c6/cz/cvut/fel/omo/cv6/state/Color.java
@@ -0,0 +1,16 @@
+package cz.cvut.fel.omo.cv6.state;
+
+/** Kurz A7B36OMO - Objektove modelovani - Cviceni 6 Design Patterns State, strategy
+ *
+ *  @author mayerto1
+ *
+ *  Traffic light colors. NO_COLOR means the traffic light is out of order.
+ */
+public enum Color {
+
+    RED,
+    ORANGE,
+    GREEN,
+    NO_COLOR
+
+}

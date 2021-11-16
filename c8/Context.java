1c7731d3e355419a083f5a37544bcb8042851de1
Shorina, Varvara <shorivar@fel.cvut.cz>
2021-11-16 16:45:54 +0000

    Upload New File

---
+++ c8/Context.java
@@ -0,0 +1,11 @@
+package cz.cvut.fel.omo.cv8;
+
+import com.google.common.collect.ImmutableList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Context {
+    
+    // TODO: implement put and get methods for storing/retrieving variables
+    
+}

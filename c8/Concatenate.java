38a29969932782d7430c7a4cfad6bf63ab388839
Shorina, Varvara <shorivar@fel.cvut.cz>
2021-11-16 16:45:32 +0000

    Upload New File

---
+++ c8/Concatenate.java
@@ -0,0 +1,12 @@
+package cz.cvut.fel.omo.cv8;
+
+public class Concatenate {
+    protected final ListExpression left;
+    protected final ListExpression right;
+
+    public Concatenate(ListExpression left, ListExpression right) {
+        this.left = left;
+        this.right = right;
+    }
+
+}

472cde9961036e5933669b2ebebf4ac3bd8bdf61
Shorina, Varvara <shorivar@fel.cvut.cz>
2021-11-16 16:46:06 +0000

    Upload New File

---
+++ c8/Remove.java
@@ -0,0 +1,12 @@
+package cz.cvut.fel.omo.cv8;
+
+public class Remove {
+    protected final ListExpression sub;
+    protected int element;
+
+    public Remove(ListExpression sub, int element) {
+        this.sub = sub;
+        this.element = element;
+    }
+
+}

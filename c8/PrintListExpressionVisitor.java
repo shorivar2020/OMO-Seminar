c50f670ed2f63109c1aaff2ca727ac86a5459f18
Shorina, Varvara <shorivar@fel.cvut.cz>
2021-11-16 16:47:39 +0000

    Update PrintListExpressionVisitor.java

--- c8/PrintListExpressionVisitor.java
+++ c8/PrintListExpressionVisitor.java
@@ -6,4 +6,15 @@ public class PrintListExpressionVisitor implements ListExpressionVisitor {
     public void visitIntList(IntList v) {
         System.out.print(v.list);
     }
+    
+    public void visitVarList(VarList v) {
+        System.out.print(v.name);
+    }
+    public void visitedElemention(Concatenate c){
+        System.out.print("c(");
+        c.left.accept(this);
+        System.out.print(", ");
+        c.right.accept(this);
+        System.out.print(")");
+    }
 }
